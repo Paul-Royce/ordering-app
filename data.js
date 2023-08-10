275a54229acf65d977cd7020ac756c0486713773
Paul <paulroycefinance@gmail.com>
2023-08-10 12:06:13 +0200

    test img path one

--- data.js
+++ data.js
@@ -3,21 +3,21 @@ export const menuArray = [
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         price: 14,
-        image: "T/img/pizza.jpg",
+        image: "img/pizza.jpg",
         uuid: "a2e2ad66-5c17-4ac5-ac43-e206cff6f785one"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        image: "T/img/hamburger.jpg",
+        image: "img/hamburger.jpg",
         uuid: "271fbefc-3863-4cfe-a552-bd6ef62747catwo"
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        image: "T/img/beer.jpg",
+        image: "img/beer.jpg",
         uuid: "387054df-c9a9-455c-8ef0-d590fbfb7b15three"
     }
 ]
